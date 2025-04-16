1db48ed9430ea67e5c9d91ab16e968bacb8dfb24
Nandro31 <silvadominguezf@gmail.com>
2025-04-16 14:22:03 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-# Configurar pantalla virtual
+# Pantalla virtual
 export DISPLAY=:1
-
-# Lanzar servidor X virtual
 Xvfb :1 -screen 0 1280x720x24 &
 sleep 2
 
-# Lanzar bus de sesiones
+# Dbus para que no falle entorno
 eval $(dbus-launch)
 
-# Lanzar XFCE en segundo plano
-startxfce4 &
-sleep 3
+# Lanzar algo visible aunque XFCE no funcione
+xterm -geometry 80x24+200+200 -bg black -fg green &
 
-# Lanzar VNC conectado a Xvfb
+# VNC
 x11vnc -display :1 -nopw -forever -shared -bg
 
-# Abrir navegador apuntando a ChatGPT
-chromium-browser --no-sandbox --no-first-run --disable-infobars --app=https://chat.openai.com &
-
-# Iniciar NoVNC y dejarlo corriendo
-exec /home/remoteuser/noVNC/utils/websockify/run --web /home/remoteuser/noVNC 80 localhost:5900
+# NoVNC
+/home/remoteuser/noVNC/utils/websockify/run --web /home/remoteuser/noVNC 80 localhost:5900
