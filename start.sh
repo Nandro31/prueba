14f41a83b78018fb3672ccd026607631df3defc2
Nandro31 <silvadominguezf@gmail.com>
2025-04-16 14:28:09 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
 export DISPLAY=:1
+
+# Lanzar X virtual
 Xvfb :1 -screen 0 1280x720x24 &
 sleep 2
 
-# D-Bus
-eval $(dbus-launch)
+# Lanzar fondo gráfico (opcional, visible)
+xsetroot -solid grey &
 
-# Lanzar escritorio XFCE
-startxfce4 &
-
-sleep 5
-
-# Abrir ChatGPT en Chromium
+# Abrir navegador con ChatGPT
 chromium-browser --no-sandbox --no-first-run --disable-infobars --app=https://chat.openai.com &
 
-# Lanzar VNC conectado a la pantalla virtual
+# Lanzar VNC sobre :1
 x11vnc -display :1 -nopw -forever -shared -bg
 
-# NoVNC expuesto por web
+# Lanzar NoVNC
 exec /home/remoteuser/noVNC/utils/websockify/run --web /home/remoteuser/noVNC 80 localhost:5900
